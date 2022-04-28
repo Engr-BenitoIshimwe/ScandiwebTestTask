85f9003cfd8546f1722ed721d84e937bdd5a4daf
Benito Ishimwe <73600412+Benish-31@users.noreply.github.com>
2022-04-28 22:54:03 +0200

    Update header.php
    Changed Title

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="303510.svg" type="image/svg+xml">
     <link rel="stylesheet" href="style.css">
-    <title>PHP MYSQL PRODUCT ADD | BENITO DEVELOPER</title>
+    <title>PRODUCT ADD | BENITO DEVELOPER</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body style="color: white; background:rgb(2, 2, 26);">
