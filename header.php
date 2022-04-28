960da4badcfb19eaed025326c2d51a4f1aeaaf32
Benito Ishimwe <73600412+Benish-31@users.noreply.github.com>
2022-04-28 22:42:27 +0200

    Update header.php
    Added styles to the background

--- header.php
+++ header.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="notion_logo.svg" type="image/svg+xml">
+    <link rel="icon" href="303510.svg" type="image/svg+xml">
     <link rel="stylesheet" href="style.css">
-    <title>Junior Developer Test Task | Scandiweb</title>
+    <title>PHP MYSQL PRODUCT ADD | BENITO DEVELOPER</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-<body>
+<body style="color: white; background:rgb(2, 2, 26);">
+<span style="content: '';
+ position: fixed;
+ height: 200px;
+ width: 200px;
+ background: rgb(0, 255, 255);
+ filter: blur(140px);"></span><span style="content: '';
+ position: fixed;
+ top: 200px;
+ left: 250px;
+ height: 200px;
+ width: 200px;
+ background: rgb(183, 0, 255);
+ filter: blur(140px);"></span>
